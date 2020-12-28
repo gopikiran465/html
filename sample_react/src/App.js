8caeffaecb685c336f92e5ec9197192f5e901150
GopiKiranS <gopikirans@getongrey.com>
2020-12-28 19:41:18 +0530

    react sample

---
+++ sample_react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TableWithOutCss from './table/TableWithoutCss'
+
+function App() {
+  return (
+    <div className="App">
+      <TableWithOutCss />
+    </div>
+  );
+}
+
+export default App;
